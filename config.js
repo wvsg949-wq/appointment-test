d3e481953b36d11edf04b827cbda1b13315e88ae
Bilal Qureshi <wvsg949@gmail.com>
2026-05-12 09:40:13 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// STEP 1:
+// Paste your Supabase Project URL and Anon Key here.
+// You can find them in Supabase:
+// Project Settings -> API Keys
+
+window.SUPABASE_CONFIG = {
+  url: "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE",
+  anonKey: "PASTE_YOUR_SUPABASE_ANON_KEY_HERE"
+};
